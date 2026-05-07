a90a73dc703df043000573812a7bd6caf653f43e
Adonis-95 <mbayethiang17@gmail.com>
2026-05-07 11:54:41 +0000

    Lab 3 : API FastAPI avec /health et /predict (v2)

--- notebooks/train_model.py
+++ notebooks/train_model.py
@@ -188,8 +188,11 @@ features = [
 ]
 
 # Prédire
-diagnostic = model_loaded.predict([features])[0]
-probas = model_loaded.predict_proba([features])[0]
+
+features_df = pd.DataFrame([features], columns=feature_cols)
+
+diagnostic = model_loaded.predict(features_df)[0]
+probas = model_loaded.predict_proba(features_df)[0]
 proba_max = probas.max()
 
 print("\n--- Résultat du pré-diagnostic ---")
@@ -201,3 +204,70 @@ print("\nProbabilités par classe :")
 for classe, proba in zip(model_loaded.classes_, probas):
     bar = '#' * int(proba * 30)
     print(f"{classe:8s} : {proba:.1%} {bar}")
+
+
+# exo1 
+importances = model.feature_importances_
+
+for name, imp in sorted(zip(feature_cols, importances),
+                        key=lambda x: x[1], reverse=True):
+    print(f"{name:20s} : {imp:.3f}")
+
+# exo2 
+
+patients = [
+    {
+        'nom': 'Jeune sain',
+        'age': 20,
+        'sexe': 'M',
+        'temperature': 36.5,
+        'tension_sys': 120,
+        'toux': False,
+        'fatigue': False,
+        'maux_tete': False,
+        'region': 'Dakar'
+    },
+    {
+        'nom': 'Forte fievre',
+        'age': 35,
+        'sexe': 'F',
+        'temperature': 40.0,
+        'tension_sys': 110,
+        'toux': True,
+        'fatigue': True,
+        'maux_tete': True,
+        'region': 'Dakar'
+    },
+    {
+        'nom': 'Patient age malade',
+        'age': 65,
+        'sexe': 'M',
+        'temperature': 38.5,
+        'tension_sys': 130,
+        'toux': True,
+        'fatigue': True,
+        'maux_tete': False,
+        'region': 'Dakar'
+    }
+]
+
+for p in patients:
+    sexe_enc = le_sexe_loaded.transform([p['sexe']])[0]
+    region_enc = le_region_loaded.transform([p['region']])[0]
+
+    features = [
+        p['age'],
+        sexe_enc,
+        p['temperature'],
+        p['tension_sys'],
+        int(p['toux']),
+        int(p['fatigue']),
+        int(p['maux_tete']),
+        region_enc
+    ]
+
+    features_df = pd.DataFrame([features], columns=feature_cols)
+
+    diag = model_loaded.predict(features_df)[0]
+
+    print(f"\n{p['nom']} → Diagnostic : {diag}")
